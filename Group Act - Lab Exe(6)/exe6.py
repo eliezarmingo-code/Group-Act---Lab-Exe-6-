8d084b5a45a15cbfa7c81ca9501e894469bf6dcf
Eliezar Mingo <eliezarmingo@gmail.com>
2026-03-17 13:25:26 +0800

    Add files via upload

---
+++ Group Act - Lab Exe(6)/exe6.py
@@ -0,0 +1,20 @@
+# if statement returns true or false
+x = 10
+y = 20
+if x > y:
+    print("TRUE")
+else:
+    print("FALSE")
+print()
+
+# Collections with different data types
+print("Collections with different data types:")
+our_list = ["Mingo", "Polinag", "Peregrino", "Llovit", "Liaging","Agabbao"]
+my_tuple = (1, "Hello", 3.14, True)
+my_set = {"Inasal", "Pancit", "Sinigang", "Adobo", "Kare-kare"}
+my_dict = {"name": "Eliezar", "age": 21, "Course": "BSIT"}
+
+print("Set:", my_set)
+print("tuple:", my_tuple)
+print("list:", our_list)
+print("Dictionary:", my_dict)
